8e42bc48d96bd3fcff5b501f7dcbaa69defc3119
JeppeSRC <jeppe97rc@gmail.com>
2017-12-23 23:32:30 +0100

    vec4 operator overloads

--- src/Frodo-core/core/math/vec4.h
+++ src/Frodo-core/core/math/vec4.h
@@ -24,6 +24,24 @@ public:
 	vec4& Mul(float32 v);
 	vec4& Div(const vec4& v);
 	vec4& Div(float32 v);
+
+	inline void operator+=(const vec4& right) { Add(right); }
+	inline void operator-=(const vec4& right) { Sub(right); }
+	inline void operator*=(const vec4& right) { Mul(right); }
+	inline void operator/=(const vec4& right) { Div(right); }
+	inline void operator+=(const float32 right) { Add(right); }
+	inline void operator-=(const float32 right) { Sub(right); }
+	inline void operator*=(const float32 right) { Mul(right); }
+	inline void operator/=(const float32 right) { Div(right); }
+
+	inline vec4 operator+(const vec4& right) const { return vec4(*this).Add(right); }
+	inline vec4 operator-(const vec4& right) const { return vec4(*this).Sub(right); }
+	inline vec4 operator*(const vec4& right) const { return vec4(*this).Mul(right); }
+	inline vec4 operator/(const vec4& right) const { return vec4(*this).Div(right); }
+	inline vec4 operator+(const float32 right) const { return vec4(*this).Add(right); }
+	inline vec4 operator-(const float32 right) const { return vec4(*this).Sub(right); }
+	inline vec4 operator*(const float32 right) const { return vec4(*this).Mul(right); }
+	inline vec4 operator/(const float32 right) const { return vec4(*this).Div(right); }
 };
 
 class vec4d {
@@ -45,6 +63,24 @@ public:
 	vec4d& Mul(float64 v);
 	vec4d& Div(const vec4d& v);
 	vec4d& Div(float64 v);
+
+	inline void operator+=(const vec4d& right) { Add(right); }
+	inline void operator-=(const vec4d& right) { Sub(right); }
+	inline void operator*=(const vec4d& right) { Mul(right); }
+	inline void operator/=(const vec4d& right) { Div(right); }
+	inline void operator+=(const float64 right) { Add(right); }
+	inline void operator-=(const float64 right) { Sub(right); }
+	inline void operator*=(const float64 right) { Mul(right); }
+	inline void operator/=(const float64 right) { Div(right); }
+
+	inline vec4d operator+(const vec4d& right) const { return vec4d(*this).Add(right); }
+	inline vec4d operator-(const vec4d& right) const { return vec4d(*this).Sub(right); }
+	inline vec4d operator*(const vec4d& right) const { return vec4d(*this).Mul(right); }
+	inline vec4d operator/(const vec4d& right) const { return vec4d(*this).Div(right); }
+	inline vec4d operator+(const float64 right) const { return vec4d(*this).Add(right); }
+	inline vec4d operator-(const float64 right) const { return vec4d(*this).Sub(right); }
+	inline vec4d operator*(const float64 right) const { return vec4d(*this).Mul(right); }
+	inline vec4d operator/(const float64 right) const { return vec4d(*this).Div(right); }
 };
 
 class vec4i {
@@ -66,6 +102,24 @@ public:
 	vec4i& Mul(int32 v);
 	vec4i& Div(const vec4i& v);
 	vec4i& Div(int32 v);
+
+	inline void operator+=(const vec4i& right) { Add(right); }
+	inline void operator-=(const vec4i& right) { Sub(right); }
+	inline void operator*=(const vec4i& right) { Mul(right); }
+	inline void operator/=(const vec4i& right) { Div(right); }
+	inline void operator+=(const int32 right) { Add(right); }
+	inline void operator-=(const int32 right) { Sub(right); }
+	inline void operator*=(const int32 right) { Mul(right); }
+	inline void operator/=(const int32 right) { Div(right); }
+
+	inline vec4i operator+(const vec4i& right) const { return vec4i(*this).Add(right); }
+	inline vec4i operator-(const vec4i& right) const { return vec4i(*this).Sub(right); }
+	inline vec4i operator*(const vec4i& right) const { return vec4i(*this).Mul(right); }
+	inline vec4i operator/(const vec4i& right) const { return vec4i(*this).Div(right); }
+	inline vec4i operator+(const int32 right) const { return vec4i(*this).Add(right); }
+	inline vec4i operator-(const int32 right) const { return vec4i(*this).Sub(right); }
+	inline vec4i operator*(const int32 right) const { return vec4i(*this).Mul(right); }
+	inline vec4i operator/(const int32 right) const { return vec4i(*this).Div(right); }
 };
 
 class vec4l {
@@ -87,6 +141,24 @@ public:
 	vec4l& Mul(int64 v);
 	vec4l& Div(const vec4l& v);
 	vec4l& Div(int64 v);
+
+	inline void operator+=(const vec4l& right) { Add(right); }
+	inline void operator-=(const vec4l& right) { Sub(right); }
+	inline void operator*=(const vec4l& right) { Mul(right); }
+	inline void operator/=(const vec4l& right) { Div(right); }
+	inline void operator+=(const int64 right) { Add(right); }
+	inline void operator-=(const int64 right) { Sub(right); }
+	inline void operator*=(const int64 right) { Mul(right); }
+	inline void operator/=(const int64 right) { Div(right); }
+
+	inline vec4l operator+(const vec4l& right) const { return vec4l(*this).Add(right); }
+	inline vec4l operator-(const vec4l& right) const { return vec4l(*this).Sub(right); }
+	inline vec4l operator*(const vec4l& right) const { return vec4l(*this).Mul(right); }
+	inline vec4l operator/(const vec4l& right) const { return vec4l(*this).Div(right); }
+	inline vec4l operator+(const int64 right) const { return vec4l(*this).Add(right); }
+	inline vec4l operator-(const int64 right) const { return vec4l(*this).Sub(right); }
+	inline vec4l operator*(const int64 right) const { return vec4l(*this).Mul(right); }
+	inline vec4l operator/(const int64 right) const { return vec4l(*this).Div(right); }
 };
 
 }
