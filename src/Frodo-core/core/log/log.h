5958777d7561fec1b34652bea6539fb9b4c16db1
JeppeSRC <jeppe97rc@gmail.com>
2018-02-25 10:36:24 +0100

    FD_DBG and FD_ASSERT is only used in debug builds

--- src/Frodo-core/core/log/log.h
+++ src/Frodo-core/core/log/log.h
@@ -4,11 +4,16 @@
 #include <core/enums.h>
 
 #define FD_INFO(msg, ...) Log::Info(msg, ##__VA_ARGS__)
-#define FD_DBG(msg, ...) Log::Debug(msg, ##__VA_ARGS__)
 #define FD_WARN(msg, ...) Log::Warning(msg, ##__VA_ARGS__)
 #define FD_FATAL(msg, ...) Log::Fatal(msg, ##__VA_ARGS__)
 
-#define  FD_ASSERT(x) if (!x) { FD_FATAL("Assertion failed \"%s\" FILE=\"%s\" LINE=%u FUNC=\"%s\"", #x, __FILE__, __LINE__, __FUNCSIG__); int32* abcdefghijklmnopqrstuvwxyz = nullptr; *abcdefghijklmnopqrstuvwxyz = 1;}
+#ifdef FD_DEBUG
+#define FD_DBG(msg, ...) Log::Debug(msg, ##__VA_ARGS__)
+#define FD_ASSERT(x) if (!x) { FD_FATAL("Assertion failed \"%s\" FILE=\"%s\" LINE=%u FUNC=\"%s\"", #x, __FILE__, __LINE__, __FUNCSIG__); int32* abcdefghijklmnopqrstuvwxyz = nullptr; *abcdefghijklmnopqrstuvwxyz = 1;}
+#else
+#define FD_DBG(msg, ...)
+#define FD_ASSERT(x)
+#endif
 
 namespace fd { 
 namespace core {
