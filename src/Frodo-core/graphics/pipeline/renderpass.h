dc2d5541dc38086ef33403d90f543f644703eafc
JeppeSRC <jeppe97rc@gmail.com>
2018-03-06 21:45:32 +0100

    VK: renderpass now supports subpasses

--- src/Frodo-core/graphics/pipeline/renderpass.h
+++ src/Frodo-core/graphics/pipeline/renderpass.h
@@ -4,9 +4,28 @@
 #include <utils/list.h>
 #include <graphics/texture/framebuffer.h>
 
+#define FD_MAX_ATTACHMENTS 0x08
+#define FD_NO_ATTACHMENT (~(uint32)0)
+#define FD_SWAPCHAIN_ATTACHMENT_INDEX ((~(uint32)0) - 1)
+
 namespace fd {
 namespace graphics {
 namespace pipeline {
+
+struct RenderSubPassInfo {
+	uint32 colorAttachments[FD_MAX_ATTACHMENTS];
+	uint32 depthStencilAttachment;
+
+	uint32 inputAttachments[FD_MAX_ATTACHMENTS];
+};
+
+struct RenderPassInfo {
+	utils::List<texture::Framebuffer*> framebuffers;
+
+	utils::List<RenderSubPassInfo> subpasses;
+
+
+};
 
 #ifdef FD_DX
 
@@ -19,8 +38,11 @@ private:
 	utils::List<texture::Framebuffer*> framebuffers;
 
 	utils::List<VkFramebuffer> framebufferObjects;
+
+	RenderPassInfo* info;
 public:
 	RenderPass();
+	RenderPass(const RenderPassInfo* info);
 	~RenderPass();
 
 	inline VkRenderPass GetRenderPass() const { return renderPass; }
--- src/Frodo-core/platforms/vulkan/VKpipeline_renderpass.cpp
+++ src/Frodo-core/platforms/vulkan/VKpipeline_renderpass.cpp
@@ -1,5 +1,6 @@
 #include <graphics/pipeline/renderpass.h>
 #include <core/video/context.h>
+#include <core/log/log.h>
 
 namespace fd {
 namespace graphics {
@@ -9,6 +10,23 @@ using namespace core;
 using namespace video;
 using namespace utils;
 using namespace texture;
+using namespace log;
+
+static bool CheckFramebuffers(const List<Framebuffer*>& framebuffers) {
+
+	uint32 w = framebuffers[0]->GetWidth();
+	uint32 h = framebuffers[0]->GetHeight();
+
+	for (uint_t i = 1; i < framebuffers.GetSize(); i++) {
+		if (framebuffers[i]->GetWidth() != w) {
+			return false;
+		} else if (framebuffers[i]->GetHeight() != h) {
+			return false;
+		}
+	}
+
+	return true;
+}
 
 RenderPass::RenderPass() : renderPass(nullptr) {
 
@@ -90,12 +108,208 @@ RenderPass::RenderPass() : renderPass(nullptr) {
 
 }
 
+RenderPass::RenderPass(const RenderPassInfo* info) : renderPass(nullptr), info(new RenderPassInfo), framebuffers(info->framebuffers) {
+	memcpy(this->info, info, sizeof(RenderPassInfo));
+
+	VkAttachmentDescription* attachments = new VkAttachmentDescription[framebuffers.GetSize() + 1];
+
+	uint32 swapchainIndex = framebuffers.GetSize();
+
+	VkAttachmentDescription& swapchainAttachment = attachments[swapchainIndex];
+
+	swapchainAttachment.flags = 0;
+	swapchainAttachment.format = Context::GetSwapchainFormat();
+	swapchainAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+	swapchainAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+	swapchainAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+	swapchainAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+	swapchainAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+	swapchainAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+	swapchainAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+
+	for (uint_t i = 0; i < info->framebuffers.GetSize(); i++) {
+		VkAttachmentDescription& desc = attachments[i];
+
+		desc.flags = 0;
+		desc.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		desc.finalLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		desc.format = info->framebuffers[i]->GetFormat();
+		desc.samples = VK_SAMPLE_COUNT_1_BIT;
+		desc.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		desc.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+		desc.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+		desc.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+	}
+
+	List<VkAttachmentReference> attachmentReferences;
+	List<VkSubpassDescription> subpassDescriptions;
+
+	attachmentReferences.Reserve(info->subpasses.GetSize() * FD_MAX_ATTACHMENTS + 1);
+
+	bool usesSwapchainImage = false;
+
+	for (uint_t i = 0; i < info->subpasses.GetSize(); i++) {
+		RenderSubPassInfo subInfo = info->subpasses[i];
+		
+		VkSubpassDescription subpass;
+
+		subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+		subpass.flags = 0;
+		subpass.colorAttachmentCount = ~0;
+		subpass.inputAttachmentCount = ~0;
+		subpass.preserveAttachmentCount = 0;
+		subpass.pInputAttachments = nullptr;
+		subpass.pDepthStencilAttachment = nullptr;
+		subpass.pPreserveAttachments = nullptr;
+		subpass.pResolveAttachments = nullptr;
+
+		VkAttachmentReference ref;
+
+		ref.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+		for (uint32 j = 0; j < FD_MAX_ATTACHMENTS; j++) {
+			ref.attachment = subInfo.colorAttachments[j];
+
+			if (ref.attachment == FD_NO_ATTACHMENT) {
+				subpass.colorAttachmentCount = j;
+				break;
+			} else if (ref.attachment == FD_SWAPCHAIN_ATTACHMENT_INDEX) {
+				ref.attachment = swapchainIndex;
+				usesSwapchainImage = true;
+			}
+
+			attachmentReferences.Push_back(ref);
+		}
+
+		if (subpass.colorAttachmentCount == ~0) {
+			subpass.colorAttachmentCount = FD_MAX_ATTACHMENTS;
+		} else if (subpass.colorAttachmentCount == 0) {
+			Log::Fatal("[RenderPass] No color attachment specified!");
+			return;
+		}
+
+		subpass.pColorAttachments = &attachmentReferences[attachmentReferences.GetSize() - subpass.colorAttachmentCount];
+
+		ref.layout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+
+		for (uint32 j = 0; j < FD_MAX_ATTACHMENTS; j++) {
+			ref.attachment = subInfo.inputAttachments[j];
+
+			if (ref.attachment == FD_NO_ATTACHMENT) {
+				subpass.inputAttachmentCount = j;
+				break;
+			}
+
+			attachmentReferences.Push_back(ref);
+		}
+
+		if (subpass.inputAttachmentCount == -1) {
+			subpass.inputAttachmentCount = FD_MAX_ATTACHMENTS;
+		}
+
+		subpass.pInputAttachments = &attachmentReferences[attachmentReferences.GetSize() - subpass.inputAttachmentCount];
+
+		if (subInfo.depthStencilAttachment != FD_NO_ATTACHMENT) {
+			ref.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+			ref.attachment = subInfo.depthStencilAttachment;
+
+			attachmentReferences.Push_back(ref);
+
+			subpass.pDepthStencilAttachment = &attachmentReferences[attachmentReferences.GetSize() - 1];
+		}
+
+		subpassDescriptions.Push_back(subpass);
+	}
+
+	List<VkSubpassDependency> dependencies;
+
+	VkSubpassDependency dep;
+
+	dep.srcSubpass = VK_SUBPASS_EXTERNAL;
+	dep.dstSubpass = 1;
+	dep.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+	dep.srcAccessMask = 0;
+	dep.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+	dep.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+	dep.dependencyFlags = 0;
+	
+	dependencies.Push_back(dep);
+
+	for (uint32 i = 1; i < subpassDescriptions.GetSize(); i++) {
+		dep.srcSubpass = i - 1;
+		dep.dstSubpass = i;
+
+		dependencies.Push_back(dep);
+	}
+
+	VkRenderPassCreateInfo rinfo;
+
+	rinfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+	rinfo.pNext = nullptr;
+	rinfo.flags = 0;
+	rinfo.attachmentCount = info->framebuffers.GetSize() + usesSwapchainImage ?  1 : 0;
+	rinfo.pAttachments = attachments;
+	rinfo.subpassCount = subpassDescriptions.GetSize();
+	rinfo.pSubpasses = &subpassDescriptions[0];
+	rinfo.dependencyCount = dependencies.GetSize();
+	rinfo.pDependencies = &dependencies[0];
+
+	VK(vkCreateRenderPass(Context::GetDevice(), &rinfo, nullptr, &renderPass));
+
+	VkImageView* imageViews = new VkImageView[framebuffers.GetSize() + 1];
+
+	for (uint_t i = 0; i < framebuffers.GetSize(); i++) {
+		imageViews[i] = framebuffers[i]->GetImageView();
+	}
+
+	VkFramebufferCreateInfo finfo;
+
+	finfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+	finfo.flags = 0;
+	finfo.layers = 1;
+	finfo.renderPass = renderPass;
+	finfo.pAttachments = imageViews;
+	
+	if (usesSwapchainImage) {
+		finfo.width = Context::GetSwapchainExtent().width;
+		finfo.height = Context::GetSwapchainExtent().height;
+		finfo.attachmentCount = framebuffers.GetSize() + 1;
+
+		List<VkImageView> swapViews = Context::GetSwapchainImageViews();
+
+		for (uint_t i = 0; i < swapViews.GetSize(); i++) {
+			imageViews[swapchainIndex] = swapViews[i];
+
+			VkFramebuffer framebuffer;
+
+			VK(vkCreateFramebuffer(Context::GetDevice(), &finfo, nullptr, &framebuffer));
+
+			framebufferObjects.Push_back(framebuffer);
+		}
+
+	} else {
+		CheckFramebuffers(framebuffers);
+		finfo.width = framebuffers[0]->GetWidth();
+		finfo.height = framebuffers[0]->GetHeight();
+		finfo.attachmentCount = framebuffers.GetSize();
+
+		VkFramebuffer framebuffer;
+
+		VK(vkCreateFramebuffer(Context::GetDevice(), &finfo, nullptr, &framebuffer));
+
+		framebufferObjects.Push_back(framebuffer);
+	}
+
+}
+
 RenderPass::~RenderPass() {
 	for (uint_t i = 0; i < framebufferObjects.GetSize(); i++) {
 		vkDestroyFramebuffer(Context::GetDevice(), framebufferObjects[i], nullptr);
 	}
 
 	vkDestroyRenderPass(Context::GetDevice(), renderPass, nullptr);
+
+	delete info;
 }
 
 }
