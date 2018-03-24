1311faccd0705962a898d1b514d1060fdf33bc86
JeppeSRC <jeppe97rc@gmail.com>
2018-03-24 18:20:33 +0100

    WIN: fixed vec4l

--- src/Frodo-core/core/math/vec4l.cpp
+++ src/Frodo-core/core/math/vec4l.cpp
@@ -9,16 +9,8 @@ vec4l::vec4l() : x(0), y(0), z(0), w(0) {}
 vec4l::vec4l(int64 x, int64 y, int64 z, int64 w) : x(x), y(y), z(z), w(w) {}
 
 vec4l& vec4l::Add(const vec4l& v) {
-	__int64 a = v.x;
-	__int64 b = v.y;
-	__int64 c = v.z;
-	__int64 d = v.w;
-	__m256i vxmm = _mm256_set_epi64x(d, c, b, a);
-	a = x;
-	b = y;
-	c = z;
-	d = w;
-	__m256i xmm = _mm256_set_epi64x(d, c, b, a);
+	__m256i vxmm = _mm256_set_epi64x(v.x, v.y, v.z, v.w);
+	__m256i xmm = _mm256_set_epi64x(x, y, z, w);
 	xmm = _mm256_add_epi64(xmm, vxmm);
 
 	x = M256L(xmm, 0);
@@ -30,16 +22,8 @@ vec4l& vec4l::Add(const vec4l& v) {
 }
 
 vec4l& vec4l::Sub(const vec4l& v) {
-	__int64 a = v.x;
-	__int64 b = v.y;
-	__int64 c = v.z;
-	__int64 d = v.w;
-	__m256i vxmm = _mm256_set_epi64x(d, c, b, a);
-	a = x;
-	b = y;
-	c = z;
-	d = w;
-	__m256i xmm = _mm256_set_epi64x(d, c, b, a);
+	__m256i vxmm = _mm256_set_epi64x(v.x, v.y, v.z,v. w);
+	__m256i xmm = _mm256_set_epi64x(x, y, z, w);
 	xmm = _mm256_sub_epi64(xmm, vxmm);
 
 	x = M256L(xmm, 0);
@@ -71,16 +55,8 @@ vec4l& vec4l::Div(const vec4l& v) {
 
 //int64
 vec4l& vec4l::Add(int64 v) {
-	__int64 a = v;
-	__int64 b = v;
-	__int64 c = v;
-	__int64 d = v;
-	__m256i vxmm = _mm256_set_epi64x(d, c, b, a);
-	a = x;
-	b = y;
-	c = z;
-	d = w;
-	__m256i xmm = _mm256_set_epi64x(d, c, b, a);
+	__m256i vxmm = _mm256_set_epi64x(v, v, v, v);
+	__m256i xmm = _mm256_set_epi64x(x, y, z, w);
 	xmm = _mm256_add_epi64(xmm, vxmm);
 
 	x = M256L(xmm, 0);
@@ -93,16 +69,8 @@ vec4l& vec4l::Add(int64 v) {
 
 //int64
 vec4l& vec4l::Sub(int64 v) {
-	__int64 a = v;
-	__int64 b = v;
-	__int64 c = v;
-	__int64 d = v;
-	__m256i vxmm = _mm256_set_epi64x(d, c, b, a);
-	a = x;
-	b = y;
-	c = z;
-	d = w;
-	__m256i xmm = _mm256_set_epi64x(d, c, b, a);
+	__m256i vxmm = _mm256_set_epi64x(v, v, v, v);
+	__m256i xmm = _mm256_set_epi64x(x, y, z, w);
 	xmm = _mm256_sub_epi64(xmm, vxmm);
 
 	x = M256L(xmm, 0);
