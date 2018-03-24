a8f69241374905fa0542159af415c930ebf47a89
JeppeSRC <jeppe97rc@gmail.com>
2018-03-24 12:22:52 +0100

    LNX: Added base class scope to all base class function calls because gcc is the most retarded compiler ever

--- src/Frodo-core/utils/list.h
+++ src/Frodo-core/utils/list.h
@@ -13,16 +13,16 @@ private:
 public:
 	List(uint_t reserved = 0) : std::vector<T>(reserved) { }
 
-	inline void Push_back(const T& item) { push_back(item); }
-	inline void Remove(const T& item) { for (uint_t i = 0; i < size(); i++) { if (at(i) == item) erase(begin() + i); } }
-	inline void RemoveIndex(uint_t index) { erase(begin() + index); }
+	inline void Push_back(const T& item) { std::vector<T>::push_back(item); }
+	inline void Remove(const T& item) { for (uint_t i = 0; i < std::vector<T>::size(); i++) { if (std::vector<T>::at(i) == item) std::vector<T>::erase(std::vector<T>::begin() + i); } }
+	inline void RemoveIndex(uint_t index) { std::vector<T>::erase(std::vector<T>::begin() + index); }
 
-	inline void Reserve(uint_t count) { reserve(count); }
-	inline void Resize(uint_t count) { resize(count); }
+	inline void Reserve(uint_t count) { std::vector<T>::reserve(count); }
+	inline void Resize(uint_t count) { std::vector<T>::resize(count); }
 
 	inline uint_t Find(const T& item) const {
-		for (uint_t i = 0; i < size(); i++) {
-			if (at(i) == item) return i;
+		for (uint_t i = 0; i < std::vector<T>::size(); i++) {
+			if (std::vector<T>::at(i) == item) return i;
 		}
 
 		return ~0;
@@ -30,22 +30,22 @@ public:
 
 	template<typename K = T>
 	inline uint_t Find(bool(*CMP_FUNCTION)(const T& item, K& other), K& other) const {
-		for (uint_t i = 0; i < size(); i++) {
-			if (CMP_FUNCTION(at(i), other)) return i;
+		for (uint_t i = 0; i < std::vector<T>::size(); i++) {
+			if (CMP_FUNCTION(std::vector<T>::at(i), other)) return i;
 		}
 
 		return ~0;
 	}
 
-	inline T& operator[](uint_t index) { return at(index); }
-	inline const T& operator[](uint_t index) const { return at(index); }
+	inline T& operator[](uint_t index) { return std::vector<T>::at(index); }
+	inline const T& operator[](uint_t index) const { return std::vector<T>::at(index); }
 
-	inline void Clear() { clear(); }
+	inline void Clear() { std::vector<T>::clear(); }
 
-	inline uint_t GetSize() const { return size(); }
-	inline uint_t GetSizeInBytes() const { return size() * sizeof(T); }
-	inline T*	  GetData() { return  data(); }
-	inline T*	  GetData() const { return (T*)&at(0); }
+	inline uint_t GetSize() const { return std::vector<T>::size(); }
+	inline uint_t GetSizeInBytes() const { return std::vector<T>::size() * sizeof(T); }
+	inline T*	  GetData() { return  std::vector<T>::data(); }
+	inline T*	  GetData() const { return (T*)&std::vector<T>::at(0); }
 };
 
 }}
