1bb1d43fe60ea2e2538d32c42b8b12c1cb80c91c
JeppeSRC <jeppe97rc@gmail.com>
2018-03-24 18:23:56 +0100

    WIN: corrected vec3l

--- src/Frodo-core/core/math/vec3l.cpp
+++ src/Frodo-core/core/math/vec3l.cpp
@@ -9,14 +9,8 @@ vec3l::vec3l() : x(0), y(0), z(0) {}
 vec3l::vec3l(int64 x, int64 y, int64 z) : x(x), y(y), z(z) {}
 
 vec3l& vec3l::Add(const vec3l& v) {
-	__int64 a = v.x;
-	__int64 b = v.y;
-	__int64 c = v.z;
-	__m256i vxmm = _mm256_set_epi64x(0, c, b, a);
-	a = x;
-	b = y;
-	c = z;
-	__m256i xmm = _mm256_set_epi64x(0, c, b, a);
+	__m256i vxmm = _mm256_set_epi64x(0, v.z, v.y, v.x);
+	__m256i xmm = _mm256_set_epi64x(0, z, y, x);
 	xmm = _mm256_add_epi64(xmm, vxmm);
 
 	x = M256L(xmm, 0);
@@ -27,14 +21,8 @@ vec3l& vec3l::Add(const vec3l& v) {
 }
 
 vec3l& vec3l::Sub(const vec3l& v) {
-	__int64 a = v.x;
-	__int64 b = v.y;
-	__int64 c = v.z;
-	__m256i vxmm = _mm256_set_epi64x(0, c, b, a);
-	a = x;
-	b = y;
-	c = z;
-	__m256i xmm = _mm256_set_epi64x(0, c, b, a);
+	__m256i vxmm = _mm256_set_epi64x(0, v.z, v.y, v.x);
+	__m256i xmm = _mm256_set_epi64x(0, z, y, x);
 	xmm = _mm256_sub_epi64(xmm, vxmm);
 
 	x = M256L(xmm, 0);
@@ -63,14 +51,8 @@ vec3l& vec3l::Div(const vec3l& v) {
 
 //int64
 vec3l& vec3l::Add(int64 v) {
-	__int64 a = v;
-	__int64 b = v;
-	__int64 c = v;
-	__m256i vxmm = _mm256_set_epi64x(0, c, b, a);
-	a = x;
-	b = y;
-	c = z;
-	__m256i xmm = _mm256_set_epi64x(0, c, b, a);
+	__m256i vxmm = _mm256_set_epi64x(0, v, v, v);
+	__m256i xmm = _mm256_set_epi64x(0, z, y, x);
 	xmm = _mm256_add_epi64(xmm, vxmm);
 
 	x = M256L(xmm, 0);
@@ -82,14 +64,8 @@ vec3l& vec3l::Add(int64 v) {
 
 //int64
 vec3l& vec3l::Sub(int64 v) {
-	__int64 a = v;
-	__int64 b = v;
-	__int64 c = v;
-	__m256i vxmm = _mm256_set_epi64x(0, c, b, a);
-	a = x;
-	b = y;
-	c = z;
-	__m256i xmm = _mm256_set_epi64x(0, c, b, a);
+	__m256i vxmm = _mm256_set_epi64x(0, v, v, v);
+	__m256i xmm = _mm256_set_epi64x(0, z, y, x);
 	xmm = _mm256_sub_epi64(xmm, vxmm);
 
 	x = M256L(xmm, 0);
