c2f21e153589c8ffe12fab3be57e750cdb076bc2
JeppeSRC <jeppe97rc@gmail.com>
2018-03-24 18:16:51 +0100

    Defined uint64/int64 to __int64 on windows

--- src/Frodo-core/core/types.h
+++ src/Frodo-core/core/types.h
@@ -5,12 +5,22 @@ typedef unsigned char byte;
 typedef unsigned char uint8;
 typedef unsigned short uint16;
 typedef unsigned int uint32;
+
+#ifdef FD_WINDOWS
+typedef unsigned __int64 uint64;
+#elif FD_LINUX
 typedef unsigned long long uint64;
+#endif
 
 typedef signed char int8;
 typedef signed short int16;
 typedef signed int int32;
+
+#ifdef FD_WINDOWS
+typedef signed __int64 int64;
+#elif FD_LINUX
 typedef signed long long int64;
+#endif
 
 typedef float float32;
 typedef double float64;
