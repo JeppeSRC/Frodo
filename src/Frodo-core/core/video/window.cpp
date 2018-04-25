073b375d93fa9c916cefcba7bcefadb25f495f91
JeppeSRC <jeppe97rc@gmail.com>
2018-04-25 21:24:15 +0200

    Forgot this bitch

--- src/Frodo-core/core/video/window.cpp
+++ src/Frodo-core/core/video/window.cpp
@@ -7,6 +7,14 @@ namespace video {
 
 Window::Window(WindowCreateInfo* info) : info(info), open(false) { }
 
+Window* Window::Create(WindowCreateInfo* info) {
+#ifdef FD_WINDOWS
+	return new WINWindow(info);
+#elif FD_LINUX
+	return nullptr;
+#endif
+}
+
 }
 }
 }
