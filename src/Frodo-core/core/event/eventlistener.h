05fcc34fca4fee36065c711d3205994826a65fc2
JeppeSRC <jeppe97rc@gmail.com>
2018-05-09 22:37:34 +0200

    Added functions to eventlistener

--- src/Frodo-core/core/event/eventlistener.h
+++ src/Frodo-core/core/event/eventlistener.h
@@ -13,6 +13,19 @@ public:
 	virtual void OnWindowEvent(const WindowEvent* const event) { }
 	virtual void OnWindowEventResize(const math::vec2i& size) { }
 	virtual void OnWindowEventMove(const math::vec2i& position) { }
+	virtual void OnWindowEventFocus(bool focus) { }
+	virtual void OnWindowEventVisiblity(bool visibility) { }
+
+	virtual void OnkeyboardEvent(const KeyboardEvent* const event) { }
+	virtual void OnKeyboardEventPressed(uint32 key) { }
+	virtual void OnKeyboardEventReleased(uint32 key) {}
+	virtual void OnKeyboardEventHold(uint32 key) {}
+
+	virtual void OnMouseEvent(const MouseEvent* const event) { }
+	virtual void OnMouseEventMove(const math::vec2i& absolute, const math::vec2i& relative) { }
+	virtual void OnMouseEventPressed(uint32 button) { }
+	virtual void OnMouseEventReleased(uint32 button) { }
+	virtual void OnMouseEventHold(uint32 button) { }
 };
 
 }
