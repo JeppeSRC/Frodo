88de77fdef8801b1ee120586a7ba7f5c64fba87b
JeppeSRC <jeppe97rc@gmail.com>
2018-05-10 11:35:25 +0200

    EventListener return

--- src/Frodo-core/core/event/eventlistener.h
+++ src/Frodo-core/core/event/eventlistener.h
@@ -10,22 +10,22 @@ class EventListener {
 public:
 	virtual bool OnEvent(const Event* const event) { }
 			
-	virtual bool OnWindowEvent(const WindowEvent* const event) { }
-	virtual bool OnWindowEventResize(const math::vec2i& size) { }
-	virtual bool OnWindowEventMove(const math::vec2i& position) { }
-	virtual bool OnWindowEventFocus(bool focus) { }
-	virtual bool OnWindowEventVisiblity(bool visibility) { }
+	virtual bool OnWindowEvent(const WindowEvent* const event) { return false; }
+	virtual bool OnWindowEventResize(const math::vec2i& size) { return false; }
+	virtual bool OnWindowEventMove(const math::vec2i& position) { return false; }
+	virtual bool OnWindowEventFocus(bool focus) { return false; }
+	virtual bool OnWindowEventVisiblity(bool visibility) { return false; }
 			
-	virtual bool OnKeyboardEvent(const KeyboardEvent* const event) { }
-	virtual bool OnKeyboardEventPressed(uint32 key) { }
-	virtual bool OnKeyboardEventReleased(uint32 key) { }
-	virtual bool OnKeyboardEventHold(uint32 key) { }
+	virtual bool OnKeyboardEvent(const KeyboardEvent* const event) { return false; }
+	virtual bool OnKeyboardEventPressed(uint32 key) { return false; }
+	virtual bool OnKeyboardEventReleased(uint32 key) { return false; }
+	virtual bool OnKeyboardEventHold(uint32 key) { return false; }
 			
-	virtual bool OnMouseEvent(const MouseEvent* const event) { }
-	virtual bool OnMouseEventMove(const math::vec2i& absolute, const math::vec2i& relative) { }
-	virtual bool OnMouseEventPressed(uint32 button) { }
-	virtual bool OnMouseEventReleased(uint32 button) { }
-	virtual bool OnMouseEventHold(uint32 button) { }
+	virtual bool OnMouseEvent(const MouseEvent* const event) { return false; }
+	virtual bool OnMouseEventMove(const math::vec2i& absolute, const math::vec2i& relative) { return false; }
+	virtual bool OnMouseEventPressed(uint32 button) { return false; }
+	virtual bool OnMouseEventReleased(uint32 button) { return false; }
+	virtual bool OnMouseEventHold(uint32 button) { return false; }
 };
 
 }
